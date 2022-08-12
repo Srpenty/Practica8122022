2c994135ff6b22ee9c3414fff09c09a5292c33f2
Oscar <Oscar@LAPTOP-2H49CU0U>
2022-08-12 09:24:22 -0600

    Agregar archivos de proyecto.

---
+++ Practica8122022/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practica8122022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nota promedio: ");
+            double nota = double.Parse(Console.ReadLine());
+            if (nota >= 0 && nota <= 10)
+                if (nota >= 6.0)
+                    Console.WriteLine("Aprobado");
+                else if (nota <= 5.0)
+                    Console.WriteLine("Reposicion");
+                else
+                    Console.WriteLine("Reprobado");
+            else
+                Console.WriteLine("Nota invalida");
+
+        }
+    }
+}
